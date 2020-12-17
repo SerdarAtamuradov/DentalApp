cbc93189ee6ae05f8936c835d6bacecb5f719b1c
Serdar <serdaratamuradov24@gmail.com>
2020-12-17 12:22:59 +0300

    main

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Yhlasbek</Text>
+      <Text>Serdarbek</Text>
       <StatusBar style="auto" />
     </View>
   );
