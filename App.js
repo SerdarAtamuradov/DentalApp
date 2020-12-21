40f22f6a24dcb941d9637100ee7a111dc3bf395d
Serdar <serdaratamuradov24@gmail.com>
2020-12-21 19:25:21 +0300

    Added to module

--- App.js
+++ App.js
@@ -2,44 +2,37 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import styled from "styled-components/native";
+import { Group } from "./components";
 
 export default function App() {
   return (
-    // <View style={styles.container}>
-    //   <Text>Serdarbek</Text>
-    //   <StatusBar style="auto" />
-    // </View>
     <Container>
-      <Group>
-        <GroupTitle>11 сентября</GroupTitle>
-        <GroupItem>
-          <Avatar
-            source={{
-              uri:
-                "https://sun1.dataix-by-minsk.userapi.com/impg/cDPKK44F7WUxVhGin-cEQylnoKlX1W1pC1AfvA/kzIPgDxsNc8.jpg?size=200x0&quality=96&crop=0,175,448,448&sign=cb6e10d67235948437c05101304f06ce&ava=1",
-            }}
-          />
-        </GroupItem>
-      </Group>
+      <Group
+        title="20 декабря"
+        items={[
+          {
+            time: "15:30",
+            diagnosis: "пульпит, удаление зуба",
+            user: {
+              fullname: "Анжела Матиева",
+              avatar:
+                "https://sun2.dataix-by-minsk.userapi.com/impg/c857736/v857736470/1979ae/icAtw6izfyc.jpg?size=50x0&quality=96&crop=0,161,708,708&sign=0af826e647e9a3a1502b075846fccb94&ava=1",
+            },
+          },
+          {
+            time: "14:00",
+            diagnosis: "периодонтит",
+            user: {
+              fullname: "Василий Реактович",
+              avatar:
+                "https://sun1.dataix-by-minsk.userapi.com/impg/cDPKK44F7WUxVhGin-cEQylnoKlX1W1pC1AfvA/kzIPgDxsNc8.jpg?size=50x0&quality=96&crop=0,175,448,448&sign=0a90d5e8a93f1a922d83d6367922459a&ava=1",
+            },
+          },
+        ]}
+      ></Group>
     </Container>
   );
 }
-
-const Avatar = styled.Image`
-  border-radius: 50px;
-  width: 40px;
-  height: 40px;
-`;
-
-const GroupItem = styled.View``;
-
-const GroupTitle = styled.Text`
-  font-weight: 900;
-  font-size: 28px;
-  color: #000000;
-`;
-
-const Group = styled.View``;
 
 const Container = styled.View`
   flex: 1;
---
+++ components/Group.js
@@ -0,0 +1,80 @@
+// "https://sun2.dataix-by-minsk.userapi.com/impg/c857736/v857736470/1979ae/icAtw6izfyc.jpg?size=50x0&quality=96&crop=0,161,708,708&sign=0af826e647e9a3a1502b075846fccb94&ava=1"
+//import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+const Group = ({ title, items }) => {
+  return (
+    <GroupBlock>
+      <GroupTitle>{title}</GroupTitle>
+      {items.map((item, index) => (
+        <GroupItem key={index}>
+          <Avatar source={{ uri: item.user.avatar }} />
+          <View style={{ flex: 1 }}>
+            <FullName>{item.user.fullname}</FullName>
+            <GrayText>{item.diagnosis}</GrayText>
+          </View>
+          <GroupDate>{item.time}</GroupDate>
+        </GroupItem>
+      ))}
+    </GroupBlock>
+  );
+};
+
+Group.defaultProps = {
+  groupTitle: "Untitled",
+  items: [],
+};
+
+const GroupDate = styled.Text`
+  background: ${(props) => (props.active ? "#2A86FF;" : "#e9f5ff;")};
+  color: ${(props) => (props.active ? "#2FFF;" : "#4294ff;")};
+  border-radius: 18px;
+  font-weight: bold;
+  font-size: 14px;
+  width: 70px;
+  height: 32px;
+  text-align: center;
+  line-height: 28px;
+`;
+
+const GrayText = styled.Text`
+  font-size: 16px;
+  color: #8b979f;
+`;
+
+const FullName = styled.Text`
+  font-style: normal;
+  font-size: 16px;
+  /*font-weight: 600;*/
+  font-weight: bold;
+`;
+
+const Avatar = styled.Image`
+  border-radius: 50px;
+  width: 40px;
+  height: 40px;
+  margin-right: 15px;
+`;
+
+const GroupItem = styled.TouchableOpacity`
+  padding: 20px 0;
+  flex-direction: row;
+  align-items: center;
+  border-bottom-width: 1px;
+  border-bottom-color: #f3f3f3;
+`;
+
+const GroupTitle = styled.Text`
+  font-size: 22px;
+  /*font-weight: 800;*/
+  font-weight: bold;
+  color: #000000;
+`;
+
+const GroupBlock = styled.View`
+  padding: 0 20px;
+`;
+
+export default Group;
